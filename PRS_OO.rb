380a24a1ef6e9e9c0d39219f3411729d37d8b6c6
beersheep <beersheep0109@gmail.com>
2015-11-05 13:05:08 +0800

    seperate welcome message and get_name method, intialize player with the method

--- PRS_OO.rb
+++ PRS_OO.rb
@@ -12,14 +12,15 @@ class Player
 end
 
 class Human < Player
+  attr_accessor :choice
   def choose_hand
   
     begin
-      puts "Please select your move"
+      puts "Please select your move: [R]ock, [P]aper or [S]cissors"
       self.choice = gets.chomp.downcase
     end until Game::CHOICE.keys.include?(choice)
       self.choice = choice
-    end
+  end
 end
 
 class Computer < Player
@@ -32,15 +33,19 @@ class Game
   attr_reader :player, :computer
   CHOICE = {"r" => "Rock", "p" => "Paper", "s" => "Scissors"}
 
+
   def initialize
-    @player = Human.new("Player1")
+    @player = Human.new(ask_player_name)
     @computer = Computer.new("Computer")
   end
 
   def display_welcome_message
     puts "Welcome to the Paper, Rock, Scissors game!"
+  end
+
+  def ask_player_name
     puts "What's your name?"
-    player.name = gets.chomp
+    name = gets.chomp
   end
 
   def display_result
@@ -58,7 +63,7 @@ class Game
   def play_again?
     puts "Would you like to play again? (y/n)"
     answer = gets.chomp.downcase
-    true if answer == "y"
+    answer == "y"
   end
 
   def play
